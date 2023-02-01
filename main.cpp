83c13f9a5ec019da04b3c4a8e943c4cfb59c0206
Sol <darkmaster2210@gmail.com>
2023-02-02 00:26:45 +0100

    Changed input for testing

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 
 int main(){
 	std::cout << "-------------\nStarting Interpreter:\n";
-	std::string input = "3 * 5 ** 3 ";
+	std::string input = "function((test),test2,test3)";
 	std::cout << "Using: " << input << std::endl;
 	Lexer l;
 	std::cout << "[!] Starting Lexing:\n";
 	Tokens tokens = l.lex(input);
 	std::cout << "[o] Found these Tokens while lexing:\n";
 	for (auto token:tokens.tokens){
-		std::cout << token;
+		std::cout << token << std::endl;
 	}
 	std::cout << "[!] Starting Parsing:\n";
 	Parser p;
