4f0c913df378f785d2490f4ccf0a63c6936a28c8
Sol <darkmaster2210@gmail.com>
2023-02-05 19:28:39 +0100

    Changed some output

--- main.cpp
+++ main.cpp
@@ -6,14 +6,13 @@ int main(){
 	std::cout << "-------------\nStarting Interpreter:\n";
 	std::string input = "3 + 4 *  5 / 25 - 4 == false <= true >= 3 and 4 or 5 and not false";
 	std::cout << "Using: " << input << std::endl;
-	Lexer l;
-	std::cout << "[!] Starting Lexing:\n";
-	Tokens tokens = l.lex(input);
-	std::cout << "[o] Found these Tokens while lexing:\n";
-	for (auto token:tokens.tokens){
-		std::cout << token << std::endl;
-	}
-	std::cout << "[!] Starting Parsing:\n";
+	Lexer lexr;
+	std::cout << "[!] Starting Lexing\n";
+	Tokens tokens = lexr.lex(input);
+	//lexr.print(tokens);
+	//std::cout << "[o] Found these Tokens while lexing:\n";
+
+	std::cout << "[!] Starting Parsing\n";
 	Parser p;
 	p.parse(tokens);
 	return 0;
