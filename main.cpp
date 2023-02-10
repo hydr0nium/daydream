22b1f372c1fb6e094d61b2e89eada87abf91e935
Sol <darkmaster2210@gmail.com>
2023-02-10 14:03:49 +0100

    Changed some testing input

--- main.cpp
+++ main.cpp
@@ -4,13 +4,15 @@
 
 int main(){
 	std::cout << "-------------\nStarting Interpreter:\n";
-	std::string input = "3 + 4 *  5 / 25 - 4 == false <= true >= 3 and 4 or 5 and not false";
+	std::string input = "test_func(5+5==10, true)";
+	// Input will turn into:
+	// 3 4 5 * 25 / + 4 - false == true <= 3 4 and 5 false not and or >=
 	std::cout << "Using: " << input << std::endl;
 	Lexer lexr;
 	std::cout << "[!] Starting Lexing\n";
 	Tokens tokens = lexr.lex(input);
-	//lexr.print(tokens);
 	//std::cout << "[o] Found these Tokens while lexing:\n";
+	//lexr.print(tokens);
 
 	std::cout << "[!] Starting Parsing\n";
 	Parser p;
