59da7c58ce1ee7668575bd098e087f337b390f80
Sol <darkmaster2210@gmail.com>
2023-02-10 14:05:14 +0100

    Added parse error declaration

--- util.h
+++ util.h
@@ -13,6 +13,7 @@ std::string enum_to_string(TokenType t);
 void lex_error(int pos, std::string lexing_string);
 void lex_error(std::string error_message);
 void parse_error(std::string expected, std::string found);
+void parse_error(std::string);
 void pushOperatorToStack(std::stack<StatementHelper>& operatorStack, StatementHelper& op, std::vector<StatementHelper>& queue);
 int getOperatorPrecedence(StatementHelper& op);
 std::optional<Token> lex_keyword(std::string lexing_string, int& pos);
