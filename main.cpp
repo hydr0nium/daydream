b8548ac5aa9ab757de0c2eac6b0e2c200009d742
Sol <darkmaster2210@gmail.com>
2023-02-14 15:22:03 +0100

    Changed some stuff for testing

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "lexer.h"
 #include "parser.h"
-
+\
 int main(){
 	std::cout << "-------------\nStarting Interpreter:\n";
-	std::string input = "test_func(-5+15==10, true)";
+	std::string input = "for (i=3;i<=10;i=i+1;)\n\tif var2==5\n\t\tvar3=false\n\tend\n\n\n\n\n\nend\na=5\n\n\n\n\n\n";
 	// Input will turn into:
 	// 3 4 5 * 25 / + 4 - false == true <= 3 4 and 5 false not and or >=
-	std::cout << "Using: " << input << std::endl;
+	std::cout << "Using:\n" << input << std::endl;
 	Lexer lexr;
 	std::cout << "[!] Starting Lexing\n";
 	Tokens tokens = lexr.lex(input);
