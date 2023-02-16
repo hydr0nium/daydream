efa59e3e661075923102080316f0722dbcf4ab9a
Sol <darkmaster2210@gmail.com>
2023-02-16 13:33:28 +0100

    Added a "hack" for parsing for loops

--- reversePolish.cpp
+++ reversePolish.cpp
@@ -22,10 +22,18 @@ std::vector<StatementHelper> ReversePolishNotation::getRpnVector(){
 	return this->rpn;
 };
 
-ReversePolishNotation convertInfixToRPN(Tokens& tokens){
+ReversePolishNotation convertInfixToRPN(Tokens& tokens, Token* optional_token_beginning){
 
 	std::vector<StatementHelper> rpn_queue; // output queue in reverse polish notation
 	std::stack<StatementHelper> operatorStack;
+
+	// This is again a hacky way of adding a "nice" for loop. Basically I add a opening parenthesis in the declaration such that it looks like this:
+	// for (init;condition;<var_name>=(<statement>):
+	if(optional_token_beginning != NULL && optional_token_beginning->get_type() == LPAREN) {
+		StatementHelper helper("(", new LParen());
+		operatorStack.push(helper);
+	}
+
 	// Change while condition to be not one of the statement types
 	Token current = tokens.current();
 	bool was_operator = true; // this is a dirty way to deal with negative numbers but it works so I will leave it for now
@@ -81,14 +89,19 @@ ReversePolishNotation convertInfixToRPN(Tokens& tokens){
 			break;
 		default:
 			// Temporary Solution
-			parse_error("IDK", "stuff");
+			parse_error("Valid Statement Token", current.value, tokens);
 		}
 		current = tokens.current();
 			
 	}
+
+	current = tokens.current();
 	// Pop all operators that are still on the operatorStack to the queue
 	while(!operatorStack.empty()){
 		StatementHelper helper = operatorStack.top();
+		if (helper.type == "("){
+			parse_error(")", current.value, tokens);
+		}
 		operatorStack.pop();
 		rpn_queue.push_back(helper);
 	}
@@ -99,7 +112,9 @@ ReversePolishNotation convertInfixToRPN(Tokens& tokens){
 
 }
 
-Statement* parseRPN(ReversePolishNotation rpn){
+Statement* parseRPN(ReversePolishNotation rpn, Tokens tokens){
+
+
 	std::stack<Statement*> statement_stack;
 	for(auto helper: rpn.getRpnVector()){
 		std::string statement_type = helper.type;
@@ -118,7 +133,7 @@ Statement* parseRPN(ReversePolishNotation rpn){
 		}
 		else if (statement_type == "+") {
 			if( !(statement_stack.size()>=2) ) {
-				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form an addition object");
+				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form an addition object", tokens);
 			}
 			Statement* RHS = statement_stack.top();
 			statement_stack.pop();
@@ -130,7 +145,7 @@ Statement* parseRPN(ReversePolishNotation rpn){
 		}
 		else if (statement_type == "*") { // Build Multiplication Object
 			if ( !(statement_stack.size()>=2) ){
-				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a multiplication object");
+				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a multiplication object", tokens);
 			}
 			Statement* RHS = statement_stack.top();
 			statement_stack.pop();
@@ -143,7 +158,7 @@ Statement* parseRPN(ReversePolishNotation rpn){
 		else if (statement_type == "-") { // Build Minus Object
 			std::cout << statement_stack.top()->toTreeString() << std::endl;
 			if ( !(statement_stack.size()>=2) ){
-				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a subtraction object");
+				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a subtraction object", tokens);
 			}
 			Statement* RHS = statement_stack.top();
 			statement_stack.pop();
@@ -155,7 +170,7 @@ Statement* parseRPN(ReversePolishNotation rpn){
 		}
 		else if (statement_type == "/") { // Build Division
 			if ( !(statement_stack.size()>=2) ){
-				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a division object");
+				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a division object", tokens);
 			}
 			Statement* RHS = statement_stack.top();
 			statement_stack.pop();
@@ -167,7 +182,7 @@ Statement* parseRPN(ReversePolishNotation rpn){
 		}
 		else if (statement_type == "**") { // Build Power Object
 			if ( !(statement_stack.size()>=2) ){
-				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a power object");
+				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a power object", tokens);
 			}
 			Statement* RHS = statement_stack.top();
 			statement_stack.pop();
@@ -179,7 +194,7 @@ Statement* parseRPN(ReversePolishNotation rpn){
 		}
 		else if (statement_type == "and") { // Build logical and
 			if ( !(statement_stack.size()>=2) ){
-				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a logical and object");
+				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a logical and object", tokens);
 			}
 			Statement* RHS = statement_stack.top();
 			statement_stack.pop();
@@ -191,7 +206,7 @@ Statement* parseRPN(ReversePolishNotation rpn){
 		}
 		else if (statement_type == "not") { // Build logical not
 			if ( !(statement_stack.size()>=1) ){
-				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a logical not object");
+				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a logical not object", tokens);
 			}
 			Statement* RHS = statement_stack.top();
 			statement_stack.pop();
@@ -201,7 +216,7 @@ Statement* parseRPN(ReversePolishNotation rpn){
 		}
 		else if (statement_type == "or") { // Build logical or
 			if ( !(statement_stack.size()>=2) ){
-				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a logical or object");
+				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a logical or object", tokens);
 			}
 			Statement* RHS = statement_stack.top();
 			statement_stack.pop();
@@ -213,7 +228,7 @@ Statement* parseRPN(ReversePolishNotation rpn){
 		}
 		else if (statement_type == "<") { // Build less than
 			if ( !(statement_stack.size()>=2) ){
-				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a less than object");
+				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a less than object", tokens);
 			}
 			Statement* RHS = statement_stack.top();
 			statement_stack.pop();
@@ -225,7 +240,7 @@ Statement* parseRPN(ReversePolishNotation rpn){
 		}
 		else if (statement_type == "<=") { // Build less equal than
 			if ( !(statement_stack.size()>=2) ){
-				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a less or equal than object");
+				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a less or equal than object", tokens);
 			}
 			Statement* RHS = statement_stack.top();
 			statement_stack.pop();
@@ -237,7 +252,7 @@ Statement* parseRPN(ReversePolishNotation rpn){
 		}
 		else if (statement_type == ">") { // Build greater than
 			if ( !(statement_stack.size()>=2) ){
-				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a greater than object");
+				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a greater than object", tokens);
 			}
 			Statement* RHS = statement_stack.top();
 			statement_stack.pop();
@@ -249,7 +264,7 @@ Statement* parseRPN(ReversePolishNotation rpn){
 		}
 		else if (statement_type == ">=") { // Build greater equal than
 			if ( !(statement_stack.size()>=2) ){
-				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a greater or equal than object");
+				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a greater or equal than object", tokens);
 			}
 			Statement* RHS = statement_stack.top();
 			statement_stack.pop();
@@ -261,7 +276,7 @@ Statement* parseRPN(ReversePolishNotation rpn){
 		}
 		else if (statement_type == "==") { // Build equality
 			if ( !(statement_stack.size()>=2) ){
-				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a equality object");
+				parse_error("Could not convert RPN to AST. Not enough values on the statement stack to form a equality object", tokens);
 			}
 			Statement* RHS = statement_stack.top();
 			statement_stack.pop();
@@ -277,8 +292,12 @@ Statement* parseRPN(ReversePolishNotation rpn){
 
 
 	}
-	if (!(statement_stack.size()==1)){
-		parse_error("Could not form AST from RPN. Statement Stack had more than one element. Expected was one.");
+
+	if (statement_stack.empty()){
+		parse_error("non empty statement", "empty statement", tokens);
+	}
+	else if (!(statement_stack.size()==1)){
+		parse_error("Could not form AST from RPN. Statement Stack had more than one element. Expected was one.", tokens);
 	}
 	Statement* return_stm = statement_stack.top();
 	statement_stack.pop();
