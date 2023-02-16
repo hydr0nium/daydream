4b5637995540d2931ac19ceecd2c0eaafbcd6a7c
Sol <darkmaster2210@gmail.com>
2023-02-16 13:31:48 +0100

    Changed input programm for quick testing

--- main.cpp
+++ main.cpp
@@ -4,9 +4,7 @@
 \
 int main(){
 	std::cout << "-------------\nStarting Interpreter:\n";
-	std::string input = "for (i=3;i<=10;i=i+1;)\n\tif var2==5\n\t\tvar3=false\n\tend\n\n\n\n\n\nend\na=5\n\n\n\n\n\n";
-	// Input will turn into:
-	// 3 4 5 * 25 / + 4 - false == true <= 3 4 and 5 false not and or >=
+	std::string input = "for (i=3;i<=10;i=i+1):\n\ta=5)\nend";
 	std::cout << "Using:\n" << input << std::endl;
 	Lexer lexr;
 	std::cout << "[!] Starting Lexing\n";
