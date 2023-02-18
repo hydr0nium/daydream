c3753dcbc2493290b258cf55a0cd71f71236226c
Sol <darkmaster2210@gmail.com>
2023-02-18 22:24:47 +0100

    Changed a bit of output arround

--- main.cpp
+++ main.cpp
@@ -3,18 +3,21 @@
 #include "parser.h"
 \
 int main(){
+
+	// I just noticed that my language looks a lot like Ruby but that was unintentional. I haven't coded or seen basically any code in ruby at all.
+
 	std::cout << "-------------\nStarting Interpreter:\n";
-	std::string input = "s = 'hello world'";
+	std::string input = "while (3-2==1):\n\t3+4\nend";
 	std::cout << "Using:\n" << input << std::endl;
 	Lexer lexr;
 	std::cout << "[!] Starting Lexing\n";
 	Tokens tokens = lexr.lex(input);
-	//std::cout << "[o] Found these Tokens while lexing:\n";
-	//lexr.print(tokens);
-
-	std::cout << "[!] Starting Parsing\n";
+	std::cout << "[!] Finished Lexing\n";
 	Parser p;
-	p.parse(tokens);
+	std::cout << "[!] Starting Parsing\n";
+	Programm programm = p.parse(tokens);
+	std::cout << "[!] Finished Parsing\n";
+	std::cout << programm.toTreeString() << std::endl;
 	return 0;
 
 }
