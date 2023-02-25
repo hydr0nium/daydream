b185d9a0b06b72e38acf68734610b00b8d8c1498
Sol <darkmaster2210@gmail.com>
2023-02-26 00:45:07 +0100

    Added file reading feature

--- main.cpp
+++ main.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
 #include "lexer.h"
 #include "parser.h"
-\
-int main(){
+#include <fstream>
+#include <sstream>
+
+int main(int argc, char* argv[]){
+
+
+	// I know this is a bad way because the runtime will be bad (reading file then turning it into a string and then reading it again)
+	// but this works for now and the runtime gain will not be too much.
+	if (argc != 2) {
+		std::cout << "Usage: daydream <file>.dd\n";
+		exit(1);
+	}
+	std::ifstream fs(argv[1]);
+	if(fs.fail()) {
+		std::cout << "Could not find file with path: " << argv[1] << std::endl;
+		exit(1);
+	}
+	std::stringstream source_code;
+	source_code << fs.rdbuf();
+	std::string input = source_code.str();
+	
+
 
 	// I just noticed that my language looks a lot like Ruby but that was unintentional. I haven't coded or seen basically any code in ruby at all.
 
-	std::cout << "-------------\nStarting Interpreter:\n";
-	std::string input = "sum = 0\nfor(i = 0;i < 10; i = i+1):\n\tsum = sum + i\nend\ndebug\nsum";
-	std::cout << "Using:\n" << input << std::endl;
+	//std::cout << "-------------\nStarting Interpreter:\n";
+	//std::cout << "Source Code:\n" << input << std::endl;
 	Lexer lexr;
-	std::cout << "[!] Starting Lexing\n";
+	//std::cout << "[!] Starting Lexing\n";
 	Tokens tokens = lexr.lex(input);
-	std::cout << "[!] Finished Lexing\n";
+	//std::cout << "[!] Finished Lexing\n";
 	Parser p;
-	std::cout << "[!] Starting Parsing\n";
+	//std::cout << "[!] Starting Parsing\n";
 	Programm programm = p.parse(tokens);
 	//std::cout << programm.toTreeString() << std::endl;
-	std::cout << "[!] Finished Parsing\n";
-	std::cout << "[!] Running Programm\n\n";
+	//std::cout << "[!] Finished Parsing\n";
+	//std::cout << "[!] Running Programm\n\n";
 	programm.run();
 	std::cout << std::endl;
 	return 0;
